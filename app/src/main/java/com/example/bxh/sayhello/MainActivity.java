e3e0130b0c7fe195e3ad3963bcb50ce11f174345
buxiaohui <buxiaohui@le.com>
2016-11-26 21:04:36 +0800

    find data

--- app/src/main/java/com/example/bxh/sayhello/MainActivity.java
+++ app/src/main/java/com/example/bxh/sayhello/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.bxh.sayhello;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //testClassInit();
         //testNestedInit();
-        testAlgorithms();
+        //testAlgorithms();
+        test();
     }
 
 
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void testNestedInit(){
+    private void testNestedInit() {
         System.out.println("classTest -----我是start分割线----");
         new EnclosingClass();
         System.out.println("classTest -----我是分割线----");
@@ -35,8 +36,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void testAlgorithms(){
+    private void testAlgorithms() {
         double result = TestAlgorithms.test02(3);
-        System.out.println("TestAlgorithms-----result="+result);
+        System.out.println("TestAlgorithms-----result=" + result);
+    }
+
+    private void test() {
+        int[][] array = new int[30][40];
+        array[0][0] = 0;
+        for (int i = 0; i < array.length; i++) {
+            int start = array[0][0]+i;
+            for (int z = 0; z < array[0].length; z++) {
+                array[i][z] = start + z;
+                String out = array[i][z]<10?array[i][z]+" ,":array[i][z]+",";
+                System.out.print(out);
+                if(z==array[0].length-1){
+                    System.out.println();
+                }
+            }
+        }
+        boolean x0 = TestAlgorithms.isInside01(37, array);
+        boolean x1 = TestAlgorithms.isInside02(37, array);
     }
 }
--- app/src/main/java/com/example/bxh/sayhello/TestAlgorithms.java
+++ app/src/main/java/com/example/bxh/sayhello/TestAlgorithms.java
@@ -1,5 +1,6 @@
 package com.example.bxh.sayhello;
 
+import android.os.SystemClock;
 import android.util.Log;
 
 /**
@@ -9,6 +10,61 @@ import android.util.Log;
 public class TestAlgorithms {
     private final static String TAG = "TestAlgorithms";
 
+    public static double test02(int N) {
+        Log.d(TAG, "test01 N=" + N);
+        if (N - 1 >= 0) {
+            return Math.log(N) + test02(N - 1);
+        }
+        return 0;
+    }
+
+    public static boolean isInside01(int target, int[][] array) {
+        //final long startT = System.currentTimeMillis();
+        final long startT = System.nanoTime();
+        System.out.println("isInside01 start:" + startT);
+        if (array == null) {
+            return false;
+        } else if (!(array.length > 0)) {
+            return false;
+        } else {
+            int tx = 0;
+            int ty = array.length - 1;
+            while (tx >= 0 && tx < array[0].length && ty >= 0 && ty < array.length) {
+                if (target < array[tx][ty]) {
+                    ty--;
+                } else if (target > array[tx][ty]) {
+                    tx++;
+                } else {
+                    System.out.println("isInside01 total time:" + (System.nanoTime() - startT));
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean isInside02(int target, int[][] array) {
+        long startT = System.nanoTime();
+        System.out.println("isInside02 start:" + startT);
+        if (array == null) {
+            return false;
+        } else if (!(array.length > 0)) {
+            return false;
+        } else {
+            for (int x = array.length - 1; x >= 0; x--) {
+                for (int y = array[0].length - 1; y >= 0; y--) {
+                    int temp = array[x][y];
+                    if (target == temp) {
+                        System.out.println("isInside02 total time:" + (System.nanoTime() - startT));
+
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
     public void test01() {
         Log.d(TAG, "onQueryTextSubmit Integer.toBinaryString=" + Integer.toBinaryString(10));
         String s = "";
@@ -16,14 +72,6 @@ public class TestAlgorithms {
             s = (n % 2) + s;
             Log.d(TAG, "onQueryTextSubmit s=" + s);
         }
-    }
-
-    public static double test02(int N) {
-        Log.d(TAG, "test01 N=" + N);
-        if (N - 1 >= 0) {
-            return Math.log(N) + test02(N - 1);
-        }
-        return 0;
     }
 
 }
