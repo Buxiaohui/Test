245ecc2982afea74062719571a001d9b58741f43
buxiaohui <buxiaohui@le.com>
2017-07-19 10:22:31 +0800

    ha
    Signed-off-by: buxiaohui <buxiaohui@le.com>

--- algorithms/src/main/java/com/bxh/algorithms/OtherAlgorithms/StringTest.java
+++ algorithms/src/main/java/com/bxh/algorithms/OtherAlgorithms/StringTest.java
@@ -70,4 +70,66 @@ public class StringTest {
         int x = a % 10;
         return x + getChar(a / 10);
     }
+
+    /**
+     * "123345566667"->"1234567"
+     */
+    private String delRepeatCahr() {
+        String res = "123345566667";
+        int index = 0;
+        String result = "";
+        while (index < res.length()) {
+            if (index == 0 || res.charAt(index) != res.charAt(index - 1)) {
+                result = result + res.charAt(index);
+            }
+            index++;
+        }
+        return result;
+    }
+
+    private String addBigInteger() {
+        String s1 = "123456789";
+        String s2 = "987654321";
+        StringBuilder sbf = new StringBuilder();
+        //处理字符串参数，防止以0为开头的字符串
+        s1 = s1.replaceAll("^(0+)", "").equals("") ? "0" : s1.replaceAll("^(0+)", "");
+        s2 = s2.replaceAll("^(0+)", "").equals("") ? "0" : s2.replaceAll("^(0+)", "");
+        //补位
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int maxLen = len1 > len2 ? len1 + 1 : len2 + 1;//多补一位0，这样可以避免溢出问题
+        String add0 = "";
+        if (len1 > len2) {
+            for (int i = len2; i < maxLen; i++) {
+                add0 += "0";
+            }
+            s2 = add0 + s2;
+            s1 = "0" + s1;
+        } else {
+            for (int i = len1; i < maxLen; i++) {
+                add0 += "0";
+            }
+            s1 = add0 + s1;
+            s2 = "0" + s2;
+        }
+        //根据是否进位决定插入的数字
+        boolean isCarry = false;
+        for (int i = maxLen - 1; i >= 0; i--) {
+            int singleSum = Integer.parseInt(s1.charAt(i) + "") + Integer.parseInt(s2.charAt(i) + "");
+            if (isCarry) {
+                singleSum += 1;
+            }
+            if (singleSum >= 10) {
+                isCarry = true;
+            } else {
+                isCarry = false;
+            }
+            sbf.insert(0, singleSum % 10);
+        }
+        //防止未溢出时返回的字符串以0开头
+        String res = sbf.toString().replaceAll("^0", "");
+        return res;
+
+    }
+
 }
